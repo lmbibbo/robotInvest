93f9b676be5f7c2f603288250f7ec6ba8b2415aa
luisma <luisma@lifia-4857>
2018-03-15 12:03:03 -0300

    Ver 33

---
+++ src/com/luisma/model/AnswerInstruments.java
@@ -0,0 +1,11 @@
+package com.luisma.model;
+
+import java.util.List;
+
+public class AnswerInstruments {
+	
+	private String status;
+	private List <Products> instruments;
+	
+
+}
--- src/testSwing/View.java
+++ src/testSwing/View.java
@@ -10,6 +10,8 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
 
 public class View {
 
@@ -17,7 +19,11 @@ public class View {
 	private TextField myTextField;
 	private Button button; 
 	private Button conect;
-	private JTextField connTextField;
+	private JPanel panel_1;
+	private JPanel panel_2;
+	private JPanel panel_3;
+	private JCheckBox chckbxNewCheckBox;
+	private JCheckBox chckbxNewCheckBox_1;
 
 	//private Model model;		//Joe: Model is hardwired in, 
 	//needed only if view initialises model (which we aren't doing)
@@ -29,31 +35,47 @@ public class View {
 		JFrame frame 		= new JFrame("simple MVC");
 		frame.getContentPane().add("North", new Label("counter"));
 
-		myTextField 		= new TextField();
-		myTextField.setColumns(5);
-		frame.getContentPane().add(myTextField, BorderLayout.WEST);
+		panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
 
-		connTextField = new JTextField();
-		frame.getContentPane().add(connTextField, BorderLayout.EAST);
-		connTextField.setColumns(10);
+		panel_3 = new JPanel();
+		panel_1.add(panel_3, BorderLayout.CENTER);
+		panel_3.setLayout(new BorderLayout(0, 0));
+
+		myTextField 		= new TextField();
+		panel_3.add(myTextField, BorderLayout.NORTH);
+		myTextField.setColumns(5);
+
+		panel_2 = new JPanel();
+		panel_1.add(panel_2, BorderLayout.WEST);
+		panel_2.setLayout(new BorderLayout(0, 0));
+
+		chckbxNewCheckBox = new JCheckBox("New check box");
+		chckbxNewCheckBox.setVerticalAlignment(SwingConstants.TOP);
+		panel_2.add(chckbxNewCheckBox, BorderLayout.NORTH);
+
+		chckbxNewCheckBox_1 = new JCheckBox("New check box");
+		chckbxNewCheckBox_1.setVerticalAlignment(SwingConstants.TOP);
+		panel_2.add(chckbxNewCheckBox_1, BorderLayout.CENTER);
 
 		//panel in constructor and not an attribute as doesn't need to be visible to whole class
 		JPanel panel 		= new JPanel();
 		button	 		= new Button("PressMe");
 		panel.add(button);
 		frame.getContentPane().add("South", panel);		
-		
+
 		conect = new Button("Connect");
 		conect.setActionCommand("Connect");
 		panel.add(conect);
-		
+
 		frame.addWindowListener(new CloseListener());	
-		frame.setSize(200,100);
+		frame.setSize(417,208);
 		frame.setLocation(100,100);
 		frame.setVisible(true);
 
 	} //View()
-	
+
 	public static class CloseListener extends WindowAdapter {
 		public void windowClosing(WindowEvent e) {
 			e.getWindow().setVisible(false);
@@ -73,7 +95,4 @@ public class View {
 		return conect;
 	}
 
-	public JTextField getConnTextField() {
-		return connTextField;
-	}
 }
